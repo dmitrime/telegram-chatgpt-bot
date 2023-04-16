ec64576334908466b55f65d853f676de6f671ca8
dmitrime <dmitri807@gmail.com>
2023-04-16 20:58:05 +0800

    Use KV to remember save and fetch previous replies

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,54 @@ export interface Env {
   OPENAI_CHAT_MODEL: string;
   OPENAI_CHAT_PROMPT: string;
   TELEGRAM_BOT_KEY: string;
+  BOT_CONTEXT_KV: KVNamespace;
+  BOT_CONTEXT_SIZE: number;
+}
+
+async function saveChatContext(
+  env: Env,
+  contextKey: string,
+  context: OpenAI.Message[],
+  text: string
+) {
+  if (context && context.length >= 1) {
+    // remove the system prompt
+    context.shift();
+    // forget earlier context, if it's already large enough
+    if (2 * env.BOT_CONTEXT_SIZE < context.length) {
+      context.splice(0, 2);
+    }
+    // add the latest reply
+    context.push({ role: "assistant", content: text });
+
+    if (env.BOT_CONTEXT_KV && contextKey.trim() != "") {
+      await env.BOT_CONTEXT_KV.put(
+        contextKey,
+        JSON.stringify(context).replaceAll("\\n", "")
+      );
+    }
+  }
+}
+
+async function fetchChatContext(
+  env: Env,
+  contextKey: string
+): OpenAI.Message[] {
+  let context: OpenAI.Message[] = [];
+
+  if (
+    env.BOT_CONTEXT_KV &&
+    env.BOT_CONTEXT_SIZE &&
+    env.BOT_CONTEXT_SIZE > 0 &&
+    contextKey.trim() != ""
+  ) {
+    context =
+      (await env.BOT_CONTEXT_KV.get(contextKey, {
+        type: "json",
+      })) || [];
+  }
+
+  return context;
 }
 
 export default {
@@ -28,17 +76,25 @@ export default {
     const text =
       update.message?.text ||
       update.inline_query?.query ||
-      update.chosen_inline_result?.query;
-    if (!text || text.trim() == "") {
+      update.chosen_inline_result?.query ||
+      "";
+    if (text.trim() == "") {
       return new Response(null);
     }
 
     const username =
       update.message?.from.username ||
       update.inline_query?.from.username ||
-      update.chosen_inline_result?.from.username;
+      update.chosen_inline_result?.from.username ||
+      "user";
 
-    let context: OpenAI.Message[] = [{ role: "user", content: text }];
+    const contextKey =
+      update.message?.chat.id == undefined
+        ? `tg_${username}_ctx`
+        : `${update.message.chat.id}`;
+    let context = await fetchChatContext(env, contextKey);
+    context.push({ role: "user", content: text });
+
     if (update.message && !update.message.via_bot) {
       const reply = await OpenAI.chatCompletions(
         env.OPENAI_API_KEY,
@@ -47,8 +103,10 @@ export default {
         `tg_${username}`,
         context
       );
+      await saveChatContext(env, contextKey, context, reply);
       return Telegram.respondMessage(reply, update.message.chat.id);
     } else if (update.inline_query) {
+      // send back whatever was typed for inline_query
       return Telegram.respondInlineQuery(text, update.inline_query.id);
     } else if (update.chosen_inline_result) {
       let newText = `*${username} said*: ` + text + "\n" + "*Bot says*: ";
@@ -65,6 +123,7 @@ export default {
         `tg_${username}`,
         context
       );
+      await saveChatContext(env, contextKey, context, reply);
 
       await Telegram.editInlineMessage(
         env.TELEGRAM_BOT_KEY,
